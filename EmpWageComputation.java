5ea3a2e6983bfd43566b3c9ffdcd4399a80edf7f
Shreesha <[shreesha2002ms@gmail.com]>
2024-09-11 11:38:06 +0530

    emp attendance funactionality

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,6 +1,19 @@
 
 class EmpWageComputation{
-	public static void main(String[] args) {
-		System.out.println("Welcome to employee wage computation");
+	
+	    public static void main(String[] args) {
+	        // Create an instance of Random class
+	        Random random = new Random();
+
+	        // Simulate attendance
+	        boolean isPresent = random.nextBoolean(); // Randomly returns true or false
+
+	        // Display attendance result
+	        if (isPresent) {
+	            System.out.println("The employee is present.");
+	        } else {
+	            System.out.println("The employee is absent.");
+	        }
+	    }
 	}
-}
+
